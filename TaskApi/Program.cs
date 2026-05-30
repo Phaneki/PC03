9c70e3f953bf95aaa303d4a9bdb3526f66dc9930
Portal Academico <dev@portalacademico.local>
2026-05-29 21:21:34 -0500

    feat: initialize TaskApi project and configure dependencies

--- TaskApi/Program.cs
+++ TaskApi/Program.cs
@@ -5,7 +5,11 @@ using TaskApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddHttpClient<ITareasExternasService, TareasExternasService>();
 builder.Services.AddSingleton<ITareaMlService, TareaMlService>();
 builder.Services.AddDbContext<TaskDbContext>(options =>
